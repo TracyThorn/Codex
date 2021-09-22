d13d5f92f860ccedcab63e75485d56bfc31759e2
Tracy Thorn <ardiosomega@gmail.com>
2021-09-22 18:16:18 -0500

    Can now display json data from a fetch request

--- React/fetchAPI/fetchapi/src/App.js
+++ React/fetchAPI/fetchapi/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const getData = () => {
+  return axios('https://jsonplaceholder.typicode.com/posts')
+    .then(({data}) => {
+      console.log(data);
+      return JSON.stringify(data);
+    })
+    .catch(err => console.error(err));
+}
+
 function App() {
+  const [postData, setPostData] = useState('');
+
+  useEffect(() => {
+    return getData().then((posts => {
+      setPostData(posts);
+    }))
+  }, [])
+
   return (
     <div className="App">
       <h1>Fetchin' Data</h1>
+      <p>
+        {postData}
+      </p>
     </div>
   );
 }
